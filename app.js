1c7fac3ed40d1f68cbed764450ddea194de61188
gomezaldana <aldana0gomez@gmail.com>
2026-04-09 23:46:17 -0300

    Iniciando proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
+import peliculaRoutes from "./src/routes/peliculasRoutes.js";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/peliculas", peliculaRoutes);
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API Cinear");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
